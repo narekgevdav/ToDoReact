68b6dc0b38579af226c7251ec4bba6d8cb6544e1
narekgevdav <49719556+narekgevdav@users.noreply.github.com>
2019-07-11 17:01:57 +0400

    Add files via upload

---
+++ precache-manifest.3114e401eae0cbf38f3abb82c4a7c507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4621fae76e891d88bc72d421abaa31c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0df7e69cbe7960a155c9",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4bd1d4eea210ddd5bfad",
+    "url": "./static/js/2.ea93b2ed.chunk.js"
+  },
+  {
+    "revision": "0df7e69cbe7960a155c9",
+    "url": "./static/js/main.430beeb7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
